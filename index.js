2bb280269b0fe81de5c644181dba83c5a22868bf
Cesar <139836008+cesarf95@users.noreply.github.com>
2025-09-17 22:20:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//Criar uma variavel chamada 'nome' e mostre no console: "Ola, meu nome é..." usando templatestring
+/*
+let nome = 'Cesar'
+console.log (`Olá, meu nome é ${nome}`)
+*/
+//TemplateString comeca com ``
+
+//Crie duas variaveis de numeros e mostre a soma
+
+/*let num1 = 10;
+let num2 = 5;
+let soma = num1 + num2
+console.log(`A soma de ${num1} + ${num2} é ${soma}`);
+*/
+//calcule o dobro de um numero armazenado em uma variavel
+
+/*let numeroo1 = 20;
+let mult = numeroo1 * 2;
+console.log(`o dobro de ${numeroo1} é ${mult}`)
+*/
+// Verifique se um numero é par ou impar
+/*
+const numero1 = '1'
+if (numero1 % 2 === 0)
+{
+console.log(`o ${numero1} é par`)
+}
+else
+ {
+ console.log(`o ${numero1} é impar`)
+} 
+//Se todo numero teminar com 1 e impar, se for com 0 e par
+*/
+
+//Verificar se uma pessoa e maior de idade
+/*
+let idade = 18
+
+if (idade >= 18){
+    
+    console.log(`voce tem ${idade} voce e maior de idade`)
+}
+else {
+        console.log(`voce tem ${idade} voce e menor de idade`)
+    }
+    */
+   
+    let nota = 7
+    if(nota <= 4.9) {
+        console.log(`O aluno com a ${nota} esta reprovado`)
+    }
+    else if (nota >= 5 && nota <= 6.9) {
+        console.log(`o aluno com a nota ${nota} esta de recuperacao`)
+    }
+     else {
+        console.log(`ò aluno com a ${nota} esta aprovado`)
+     } 
+     /* Operador ternario ? :
+     const idade2 = 18;
+     const maioridade2 = (idade2 >= 18) ? `voce tem $ {idade2} e é maior de idade` : 'Menor de idade';
+     console.log(maioridade2) */
+
+
